692e2a79170193c229c5417a1a18bc57774cf910
juiceboxbomber <juiceboxbomber@gmail.com>
2026-08-03 21:23:28 -0500

    feat: add ForgeCAD project model

---
+++ forgecad/__init__.py
@@ -0,0 +1,9 @@
+"""Public ForgeCAD API."""
+
+from .project import ApplicationType, DisplayUnits, Project
+
+__all__ = [
+    "ApplicationType",
+    "DisplayUnits",
+    "Project",
+]
---
+++ tests/test_project.py
@@ -0,0 +1,64 @@
+import pytest
+
+from forgecad import ApplicationType, DisplayUnits, Project
+from forgecad.fabrication import Material, TubeProfile
+
+
+def test_project_defaults():
+    project = Project("My Chassis")
+
+    assert project.name == "My Chassis"
+    assert project.application is ApplicationType.GENERAL
+    assert project.display_units is DisplayUnits.MILLIMETERS
+    assert project.frame.node_count == 0
+    assert project.frame.member_count == 0
+
+
+def test_project_application_selection():
+    project = Project(
+        name="Crawler",
+        application=ApplicationType.ROCK_CRAWLER,
+    )
+
+    assert project.application is ApplicationType.ROCK_CRAWLER
+
+
+def test_project_display_units_do_not_change_internal_units():
+    project = Project(
+        name="Imperial Display",
+        display_units=DisplayUnits.INCHES,
+    )
+
+    assert project.display_units is DisplayUnits.INCHES
+
+
+def test_project_owns_tube_library():
+    project = Project("Tube Test")
+    profile = TubeProfile(44.45, 3.048)
+
+    project.tube_library.add("1.750 x .120 DOM", profile)
+
+    assert project.active_profile_name == "1.750 x .120 DOM"
+    assert project.tube_library.active_profile is profile
+
+
+def test_project_accepts_default_material():
+    steel = Material(
+        name="A513 Type 5 DOM",
+        density=7850.0,
+        yield_strength=350.0,
+    )
+
+    project = Project(
+        name="Steel Frame",
+        default_material=steel,
+    )
+
+    assert project.default_material is steel
+
+
+@pytest.mark.parametrize("name", ["", "   ", "\t"])
+def test_project_name_cannot_be_empty(name):
+    with pytest.raises(ValueError):
+        Project(name)
+        
