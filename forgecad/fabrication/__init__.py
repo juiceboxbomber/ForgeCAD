ddd815c8d1699664f9595927db2effd3b8e3d19b
juiceboxbomber <juiceboxbomber@gmail.com>
2026-08-03 20:59:06 -0500

    feat: add tube profile library

--- forgecad/fabrication/__init__.py
+++ forgecad/fabrication/__init__.py
@@ -5,9 +5,11 @@ from .material import Material
 from .member import Member
 from .node import Node
 from .tube_profile import TubeProfile
+from .tube_library import TubeLibrary
 
 __all__ = [
     "Frame",
+    
     "Material",
     "Member",
     "Node",
