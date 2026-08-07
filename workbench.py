e3601daad4ccf8ad3d31d9cf96294fce3883e663
juiceboxbomber <juiceboxbomber@gmail.com>
2026-08-06 22:44:25 -0500

    cleanup: restore layout definition command and remove demo tools

--- workbench.py
+++ workbench.py
@@ -11,21 +11,17 @@ class ForgeCADWorkbench(FreeCADGui.Workbench):
     Icon = ""
 
     def Initialize(self):
-        from forgecad.adapters.freecad.commands.create_demo_frame import (
-            COMMAND_NAME as DEMO_COMMAND,
-            register_command as register_demo_command,
-        )
         from forgecad.adapters.freecad.commands.new_project import (
             COMMAND_NAME as NEW_PROJECT_COMMAND,
             register_command as register_new_project_command,
         )
-        from forgecad.adapters.freecad.commands.generate_frame import (
-            COMMAND_NAME as GENERATE_FRAME_COMMAND,
-            register_command as register_generate_frame_command,
-        )
         from forgecad.adapters.freecad.commands.draw_layout_line import (
             COMMAND_NAME as DRAW_LAYOUT_LINE_COMMAND,
             register_command as register_draw_layout_line_command,
+        )
+        from forgecad.adapters.freecad.commands.define_layout_lines import (
+            COMMAND_NAME as DEFINE_LAYOUT_LINES_COMMAND,
+            register_command as register_define_layout_lines_command,
         )
         from forgecad.adapters.freecad.commands.generate_from_selection import (
             COMMAND_NAME as GENERATE_FROM_SELECTION_COMMAND,
@@ -33,16 +29,14 @@ class ForgeCADWorkbench(FreeCADGui.Workbench):
         )
 
         register_new_project_command()
-        register_demo_command()
-        register_generate_frame_command()
         register_draw_layout_line_command()
+        register_define_layout_lines_command()
         register_generate_from_selection_command()
 
         commands = [
             NEW_PROJECT_COMMAND,
-            DEMO_COMMAND,
-            GENERATE_FRAME_COMMAND,
             DRAW_LAYOUT_LINE_COMMAND,
+            DEFINE_LAYOUT_LINES_COMMAND,
             GENERATE_FROM_SELECTION_COMMAND,
         ]
 
@@ -57,3 +51,4 @@ class ForgeCADWorkbench(FreeCADGui.Workbench):
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
+    
